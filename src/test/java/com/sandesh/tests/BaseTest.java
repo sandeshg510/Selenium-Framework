48b592591e2f8617ea85aeaed9349ebf6a0921cd
Sandesh Gore <sandeshgore510@gmail.com>
2026-05-26 14:20:39 +0530

    Making chrome sensible default browser

--- src/test/java/com/sandesh/tests/BaseTest.java
+++ src/test/java/com/sandesh/tests/BaseTest.java
@@ -4,13 +4,15 @@ import com.sandesh.framework.base.DriverFactory;
 import com.sandesh.framework.utils.ConfigReader;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
     @BeforeMethod
     @Parameters("browser")
-    public void setUp(String browser) {
+    public void setUp(@Optional("chrome")
+                      String browser) {
         DriverFactory.initDriver(browser);
         DriverFactory.getDriver()
                      .get(ConfigReader.get("baseUrl"));
